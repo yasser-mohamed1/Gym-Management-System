9a11c4612b0da3d5348a628bc0b17631efcf51ea
yasser-mohamed1 <y4ssermoh4med@gmail.com>
2024-12-23 09:37:11 +0200

    refactor: remove unused Class1 and add Result class for better error handling

---
+++ src/GymManagement.Domain/Result.cs
@@ -0,0 +1,25 @@
+﻿namespace GymManagement.Domain;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; private set; }
+    public string Error { get; private set; }
+    public T Value { get; private set; }
+
+    private Result(bool isSuccess, T value, string error)
+    {
+        IsSuccess = isSuccess;
+        Value = value;
+        Error = error;
+    }
+
+    public static Result<T> Success(T value)
+    {
+        return new Result<T>(true, value, null!);
+    }
+
+    public static Result<T> Failure(string error)
+    {
+        return new Result<T>(false, default!, error);
+    }
+}
